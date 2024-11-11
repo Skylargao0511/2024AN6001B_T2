ec1930d398930f70fb7190a5cd4d4dd71d2576ad
Skylargao0511 <151333275+Skylargao0511@users.noreply.github.com>
2024-11-11 12:37:40 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
 from flask import Flask
+from flask import render_template, request
+
+app = Flask("__name__")
+
+@app.route("/",methods = ["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__": #"__"(double underscore):magic(system)
+    app.run()
+    
+# drive→colab
+# github→codespaces
